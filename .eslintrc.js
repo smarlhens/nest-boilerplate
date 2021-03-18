5d5f861d0ceed021bfea7e26d812ab20d24bad13
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-03-18 16:58:07 +0100

    chore(deps): update dependencies, ci job, dependabot config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
+  extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   root: true,
   env: {
     node: true,
