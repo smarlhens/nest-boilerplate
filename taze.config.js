65499bcd2d317498e4ebd57047ea5ec4d23be791
Samuel MARLHENS <samuel.marlhens@gmail.com>
2025-08-18 20:47:34 +0200

    chore: update patch & minor deps

---
+++ taze.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'taze';
+
+export default defineConfig({
+  exclude: [],
+  force: true,
+  includeLocked: true,
+  install: true,
+  nodecompat: true,
+  packageMode: {
+    '@types/node': 'minor',
+    typescript: 'ignore', // todo: bump with eslint
+    eslint: 'minor', // TODO: migrate config
+    '@nestjs/*': 'minor', // breaking changes
+  },
+});
